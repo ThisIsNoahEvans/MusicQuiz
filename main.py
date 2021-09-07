d3382d88cb7499ba29e1481d53dd2b41bed2448d
thisisnoahevans <noah@itsnoahevans.co.uk>
2021-09-07 15:45:28 +0100

    Added: Clear screen function to run at every new function Saving user data & user ID Main menu backbone Settings menu (not functional)

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import json
+import os
+import sys
+
+def clearScreen():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 # Check if setup needs to be run.
 # Else, present the main menu.
@@ -17,6 +22,7 @@ def launch():
 # Will run on first launch
 # Prints welcome message, creates profile, and marks setup as complete
 def setup() :
+    clearScreen()
     print('--- WELCOME TO MUSIC QUIZ! ---\nReady to test your music knowledge?\n\n')
     print('To set up your profile, we need a name and PIN code from you. This will keep your progress private so only you can play Music Quiz.')
     createProfile()
@@ -43,18 +49,68 @@ def createProfile():
         except: # Error with value - likely entered non-numeric characters
             print('You didn\'t enter a valid PIN code. Try again.')
     
-    config = open('config.json', 'w') # Open the config file
-    data = json.load(config) # Load the file contents as JSON
-    data['users']
+    ## Get the JSON user data
+    userData = open('user-data.json', 'r') # Open the user data file
+    dataJSON = json.load(userData) # Load the file contents as JSON
+    userData.close() # Close the JSON file
+
+    ## Get the new user's ID from the config JSON file
+    config = open('config.json', 'r') # Open the config file
+    configJSON = json.load(config) # Load the file contents as JSON
+    config.close() # Close the JSON file
+    userID = int(configJSON['lastUserID']) + 1 # Add 1 to the previous user ID
+
+    dataJSON['users'].append({"id":userID, "name":name, "topScore":0, "gamesPlayed":0, "pin":pin, "correctSongs":[]}) # Add the user data to the JSON array
+    
+    # Save the new lastUserID
+    configJSON['lastUserID'] = userID # Update the last user ID in the config JSON
+    config = open('config.json', 'w') # Open the config file in write
+    json.dump(configJSON, config) # Dump the updated JSON data
+    config.close() # Close the JSON file
+
+    # Save the updated user data (with new user)
+    userData = open('user-data.json', 'w') # Open the user data file in write
+    json.dump(dataJSON, userData) # Dump the updated JSON data
+    userData.close() # Close the JSON file
     
     print(f'Welcome, {name}! Let\'s get you into your first game.')
 
 # The main menu
 # To be displayed after setup/a game
 def mainMenu(name):
-    print(f'--- MAIN MENU ---\nHi, {name}!\nChoose a playlist:\n1 - Pop\n2 - Alternative\n3 - Rock')
+    clearScreen()
+    print(f'--- MAIN MENU ---\nHi, {name}!\n1 - New Game\n2 - Settings\n3 - Add Player\n4 - Quit')
+    menuInput = False
+    while not menuInput:
+        try:
+            choice = int(input(':: Enter an option: '))
+            if not 1 <= choice <= 4:
+                print('That isn\'t a valid option. Try again.')
+            else:
+                menuInput = True
+        except:
+            print('That isn\'t a valid option. Try again.')
+    
+    if choice == 1:
+        pass
+    elif choice == 2:
+        # Settings
+        settings()
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        clearScreen()
+        print('Goodbye! Thank you for playing.')
+        sys.exit()
+
+
+def settings():
+    clearScreen()
+    print(f'--- SETTINGS ---\nChoose an option:\n1 - Configure Game Settings\n2 - Delete Player\n3 - Remove All Data\n4 - About')
+
 
 # Run the initial launch sequence
 # when the Python file is run
 launch()
+
 
